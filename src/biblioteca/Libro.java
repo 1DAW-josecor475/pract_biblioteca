b10ad1051fdd38a12e2024945d60497dc5551982
1DAW-josecor475 <jcorfer910@g.educaand.es>
2025-01-27 13:42:16 +0100

    Update Libro.java

--- src/biblioteca/Libro.java
+++ src/biblioteca/Libro.java
@@ -1,6 +1,6 @@
 package biblioteca;
 
-public class Libro {
+ public class Libro {
     private String autor;
     private String titulo;
     private int ejemplaresVendidos;
@@ -11,92 +11,80 @@ public class Libro {
         this.autor = autor;
         this.titulo = titulo;
         this.ejemplaresVendidos = 0;
-        this.secuela = null;
-        this.precuela = null;
     }
 
     public String obtenerInformacion() {
-        return "El libro " + this.titulo + " de " + this.autor + " ha vendido " + this.ejemplaresVendidos + " ejemplares.";
+        return String.format("El libro '%s' de %s ha vendido %d ejemplares.", titulo, autor, ejemplaresVendidos);
     }
 
     public void registrarVenta(int cantidad) {
-        this.ejemplaresVendidos += cantidad;
+        if (cantidad > 0) {
+            this.ejemplaresVendidos += cantidad;
+        }
     }
 
     public void obtenerTituloPrecuela() {
-        String tituloPrecuela = (this.precuela == null) ? "<NINGUNA>" : this.precuela.titulo;
-        System.out.println("* La precuela de " + this.titulo + " es: " + tituloPrecuela);
+        System.out.println("* La precuela de '" + titulo + "' es: " + 
+            (precuela != null ? precuela.titulo : "<NINGUNA>"));
     }
 
     public void obtenerTituloSecuela() {
-        String tituloSecuela = (this.secuela == null) ? "<NINGUNA>" : this.secuela.titulo;
-        System.out.println("* La secuela de " + this.titulo + " es: " + tituloSecuela);
+        System.out.println("* La secuela de '" + titulo + "' es: " + 
+            (secuela != null ? secuela.titulo : "<NINGUNA>"));
     }
 
     public void mostrarResumenSaga() {
+        Libro primerLibro = obtenerPrimerLibro();
         int totalVentas = 0;
-        int numeroLibro = 0;
-        int posicionLibroActual = 0;
-        Libro libroActual = this;
-
-        // Encontrar el primer libro de la saga
-        libroActual = obtenerPrimerLibro(libroActual);
+        int posicion = 0;
+        int numeroLibro = 1;
 
         System.out.println("*** SAGA DE LIBROS ***");
-        // Sumatorio de ejemplares vendidos y posicion del libro actual
-        while (libroActual != null) {
-            totalVentas += libroActual.ejemplaresVendidos;
-            numeroLibro++;
-            if (libroActual == this) {
-                posicionLibroActual = numeroLibro;
+        for (Libro actual = primerLibro; actual != null; actual = actual.secuela, numeroLibro++) {
+            totalVentas += actual.ejemplaresVendidos;
+            if (actual == this) {
+                posicion = numeroLibro;
             }
-            libroActual = libroActual.secuela;
         }
 
-        System.out.println("* El libro " + this.titulo + " es el libro número " + posicionLibroActual + " de una saga con un total de " + numeroLibro + " libros. Entre todos han vendido un total de " + totalVentas + " ejemplares.");
+        System.out.printf("* El libro '%s' es el libro número %d de una saga con un total de %d libros. Entre todos han vendido un total de %d ejemplares.%n",
+                titulo, posicion, numeroLibro - 1, totalVentas);
     }
 
     public void agregarSecuela(Libro secuela) {
-        this.secuela = secuela;
-        secuela.precuela = this;
+        if (secuela != null) {
+            this.secuela = secuela;
+            secuela.precuela = this;
+        }
     }
 
     public Libro[] obtenerSaga() {
-        int totalLibros = 1;
-        Libro libroActual = this;
-
-        // Recorrer la lista hasta el inicio para el sumatorio total de libros
-        while (libroActual.precuela != null) {
-            libroActual = libroActual.precuela;
-            totalLibros++;
-        }
-        // Recorrer la lista desde el actual hasta el final para el sumatorio total de libros
-        libroActual = this;
-        while (libroActual.secuela != null) {
-            libroActual = libroActual.secuela;
-            totalLibros++;
-        }
+        Libro primerLibro = obtenerPrimerLibro();
+        int totalLibros = contarLibrosSaga(primerLibro);
 
         Libro[] saga = new Libro[totalLibros];
-
-        libroActual = this;
         int index = 0;
 
-        // Volvemos a buscar el primer libro de la lista
-        libroActual = obtenerPrimerLibro(libroActual);
-        // Recorremos la lista hasta el final, añadiendo cada elemento al array saga
-        while (libroActual != null) {
-            saga[index++] = libroActual;
-            libroActual = libroActual.secuela;
+        for (Libro actual = primerLibro; actual != null; actual = actual.secuela) {
+            saga[index++] = actual;
         }
 
         return saga;
     }
 
-    private Libro obtenerPrimerLibro(Libro libroActual) {
-        while (libroActual.precuela != null) {
-            libroActual = libroActual.precuela;
+    private Libro obtenerPrimerLibro() {
+        Libro actual = this;
+        while (actual.precuela != null) {
+            actual = actual.precuela;
         }
-        return libroActual;
+        return actual;
+    }
+
+    private int contarLibrosSaga(Libro primerLibro) {
+        int contador = 0;
+        for (Libro actual = primerLibro; actual != null; actual = actual.secuela) {
+            contador++;
+        }
+        return contador;
     }
 }
